fb299c487b927de8f5464bf472346fe3664525d8
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-27 14:14:12 +0200

    Truthy and Falsy values with Boolean return values

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-// String comparison order: 0123456789abcdefgh....
-log(5 > 3); // true
-log(5 > '3'); // '5' > '3' => true
-log(5 > 'hi'); // '5' > 'hi' => false
-log(5 == '3') // '5' == '3' => false
-log(5 == '5') // '5' == '5' => true
-log('hi' == 'a') // false
+test('') // false
+test(0) // false
+test(NaN) // false
+test(-0) // false
+test(0n) // false
+test(undefined) // false
+test(null) // false
 
-console.log('-------');
+function test(value) {
+  if (!value) {
+    console.log('Not Executed');
+  }
+}
 
-// Special case: the boolean is not coerced to string
-// The boolean and the string are coerced to a number
-log('true' == true); // NaN == 1 => false
-log('1' == true); // 1 == 1 => true
-log('0' == false); // 0 == 0 => true
-log('' == false); // 0 == 0 => true
-// Normal case
-log(0 == false); // 0 == 0 => true
-log(1 == true); // 1 == 1 => true
-// Other comparisons
-log(5 > true); // 5 > 1 => true
-log('5' > true); // 5 > 1 => true
-log('hi' > true); // NaN > 1 => false
+console.log('---------');
+
+// Returns the first value if truthy, second value otherwise
+log(true || ''); // true
+log(123 || 'hi'); // 123
+log(0 || 'hi'); // 'hi
+
+// Returns second value if first is truthy
+log(true && ''); // ''
+log(123 && 'hi'); // 'hi'
+log(0 && 'hi'); // 0
+
 
 function log(result) {
   console.log(result);
